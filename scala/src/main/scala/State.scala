c141f0c6dd5d170e2ade393fbd8693e1b73c5ff9
V-YOP <V-YOP@pm.me>
2022-04-28 19:52:37 +0800

    漏了一个函数

--- scala/src/main/scala/State.scala
+++ scala/src/main/scala/State.scala
@@ -38,7 +38,7 @@ case object RNG {
     val (v1, r1) = double(rng)
     val (v2, r2) = double(r1)
     val (v3, r3) = double(r2)
-    ((v1,v2,v3), r3)
+    ((v1, v2, v3), r3)
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) =
@@ -51,11 +51,14 @@ case object RNG {
 
   // 题目 6.4 及其后
   type Rand[+A] = RNG => (A, RNG)
+
   def unit[A](a: A): Rand[A] = rng => (a, rng)
+
   def map[A, B](a: Rand[A])(f: A => B): Rand[B] = rng => {
     val (v, r) = a(rng)
     (f(v), r)
   }
+
   val double$: Rand[Double] = map(nonNegativeInt)(_ / Int.MaxValue.toDouble)
 
   def map2[A, B, C](a: Rand[A], b: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
@@ -63,23 +66,30 @@ case object RNG {
     val (v2, r2) = b(r1)
     (f(v1, v2), r2)
   }
+
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => fs match {
     case Nil => (Nil, rng)
     case x :: xs => map2(x, sequence(xs))(_ :: _)(rng)
   }
+
+  def ints$(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(_.nextInt))
+
   def flatMap[A, B](a: Rand[A])(f: A => Rand[B]): Rand[B] = rng => {
     val (v, r) = a(rng)
     f(v)(r)
   }
+
   def nonNegativeLessThan(n: Int): Rand[Int] =
-    flatMap(nonNegativeInt){ i =>
+    flatMap(nonNegativeInt) { i =>
       val mod = i % n
       if (i + (n - 1) - mod >= 0) unit(mod)
       else nonNegativeLessThan(n)
     }
+
   // 编写的时候应当将flatMap理解为顺序计算，这里先计算了a，将a的结果拿出来进行进一步使用
   // 这样理解大概对一切monad都适用——对某monad进行"计算"，将结果临时取出来进行进一步操作
-  def map$[A, B](a: Rand[A])(f: A => B): Rand[B] = flatMap(a){i => unit(f(i))}
+  def map$[A, B](a: Rand[A])(f: A => B): Rand[B] = flatMap(a) { i => unit(f(i)) }
 
   def map2$[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     flatMap(ra)(a => map(rb)(b => f(a, b))) // 或者 flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
@@ -87,18 +97,21 @@ case object RNG {
 
 final case class State[S, +A](run: S => (A, S)) {
   def apply(s: S): (A, S) = run(s)
+
   // map方法就是构造一个新的State，该State为当前State执行后对结果进行一个映射
-  def map[B](f: A => B): State[S, B] = State {s =>
+  def map[B](f: A => B): State[S, B] = State { s =>
     val (r, nextS) = run(s)
     (f(r), nextS)
   }
-  def map2[B, C](b: State[S, B])(f: (A, B) => C): State[S, C] = State {s =>
-    val (r1, s1)= run(s)
+
+  def map2[B, C](b: State[S, B])(f: (A, B) => C): State[S, C] = State { s =>
+    val (r1, s1) = run(s)
     val (r2, s2) = b.run(s1)
     (f(r1, r2), s2)
   }
+
   // 先执行当前计算，将下一个值传给f，对生成的State执行计算
-  def flatMap[B](f: A => State[S, B]): State[S, B] = State {s =>
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State { s =>
     val (r1, s1) = run(s)
     f(r1).run(s1)
   }
@@ -106,12 +119,16 @@ final case class State[S, +A](run: S => (A, S)) {
 
 case object State {
   def unit[S, A](a: A): State[S, A] = State((a, _))
+
   def sequence[S, A](a: List[State[S, A]]): State[S, List[A]] = a match {
     case Nil => State((Nil, _))
     case x :: xs => x.map2(sequence(xs))(_ :: _)
   }
+
   def get[S]: State[S, S] = State(s => (s, s))
+
   def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
   def modify[S](f: S => S): State[S, Unit] = for {
     s <- get
     _ <- set(f(s))
@@ -132,14 +149,16 @@ case object StateRNG {
 
 
 sealed trait Input
+
 case object Coin extends Input
+
 case object Turn extends Input
 
-case class Machine(locked: Boolean,coins: Int, candies: Int)
+case class Machine(locked: Boolean, coins: Int, candies: Int)
 
 case object Machine {
   def operate(input: Input): State[Machine, (Int, Int)] = State { state =>
-    def deconstruct(state: Machine):((Int, Int), Machine) = ((state.coins, state.candies), state)
+    def deconstruct(state: Machine): ((Int, Int), Machine) = ((state.coins, state.candies), state)
     // 有三个参数：Input，locked，candies <= 0?
     (input, state.locked, state.candies) match {
       case (input, _, candies) if candies <= 0 => input match {
